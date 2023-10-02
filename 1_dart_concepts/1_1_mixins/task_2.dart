856c993ead45aafb5b332685c0622dc7a45005df
PavanaGauranga <116865573+IgorBond108@users.noreply.github.com>
2023-10-02 16:17:21 +0300

    Update task_2.dart

--- 1_dart_concepts/1_1_mixins/task_2.dart
+++ 1_dart_concepts/1_1_mixins/task_2.dart
@@ -1,7 +1,63 @@
+extension StringExtensions on String {
+  List<dynamic> parseLinks() {
+    List<dynamic> result = [];
+    int currentIndex = 0;
+
+    while (currentIndex < this.length) {
+      // Find the next occurrence of "http://" or "https://" or "www."
+      int linkStart = this.lastIndexOf('http://', currentIndex);
+      if (linkStart == -1) linkStart = this.indexOf('https://', currentIndex);
+      if (linkStart == -1) linkStart = this.indexOf('www.', currentIndex);
+      if (linkStart == -1) linkStart = this.indexOf('.com', currentIndex);
+
+      // int linkFinish = this.indexOf('.com', currentIndex);
+      // if (linkFinish == -1) linkFinish = this.indexOf('.com', currentIndex);
+
+      if (linkStart == -1) {
+        // No more links found, add the remaining text as plain text
+        result.add(this.substring(currentIndex));
+        break;
+      }
+
+      // Add the preceding text as plain text
+      if (linkStart > currentIndex) {
+        result.add(this.substring(currentIndex, linkStart));
+      }
+
+      // Find the end of the link (space or end of string)
+      int linkEnd = this.indexOf(' ', linkStart);
+      if (linkEnd == -1) linkEnd = this.length;
+
+      // Add the link as a string
+      result.add(Link(this.substring(linkStart, linkEnd)));
+
+      currentIndex = linkEnd;
+    }
+
+    return result;
+  }
+}
+
+class Link {
+  final String url;
+
+  Link(this.url);
+
+  @override
+  String toString() {
+    return 'Link("$url")';
+  }
+}
+
 void main() {
-  // Implement an extension on [String], parsing links from a text.
-  //
-  // Extension should return a [List] of texts and links, e.g.:
-  // - `Hello, google.com, yay` ->
-  //   [Text('Hello, '), Link('google.com'), Text(', yay')].
+  String text = 'Hello, https://www.google.com, yay';
+  List<dynamic> parsed = text.parseLinks();
+
+  for (var item in parsed) {
+    if (item is String) {
+      print('Text("$item")');
+    } else if (item is Link) {
+      print(item);
+    }
+  }
 }
